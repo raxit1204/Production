8a9ce06f97138bf9447ec8266a7468f7beb50181
raxit1204 <bhavsarraxit12@gmail.com>
2024-09-09 13:43:28 +0530

    changes1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import { asynchandler } from "../utils/async_handlers.js";
 
 const registerUser = asynchandler(async (req, res) => {
   res.status(200).json({
-    message: "OK",
+    message: "Response Ok",
   });
 });
 
